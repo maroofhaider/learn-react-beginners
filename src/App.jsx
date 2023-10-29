e65a071b9b5fa861aa6583871baae8b648a8f613
maroofhaider <maroof.haider@colwiz.com>
2023-10-29 16:24:51 +0500

    add a exercise index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import CardLayoutStyle1 from './components/props/CardLayoutStyle1'
+import ExerciseIndex from './components/ExerciseIndex'
 import '/public/assets/css/Style.css'
 
 function App() {
   return (
-    <CardLayoutStyle1 />
+    <ExerciseIndex />
+
   )
 }
 
---
+++ src/components/ExerciseIndex.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ExerciseIndex() {
+  return (
+    <section>
+        <h1>React Exercise Indexing</h1>
+        <p>Click links to naviate diffrent exersice pages</p>
+        <div className="mq-container">
+            <ul>
+                <li>Props</li>
+                <li>Ternary Oparators List</li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default ExerciseIndex

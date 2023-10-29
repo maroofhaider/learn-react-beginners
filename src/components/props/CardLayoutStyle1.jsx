757f2bb5d32d01143ae5e5f228a06e3126683836
maroofhaider <148911718+maroofhaider@users.noreply.github.com>
2023-10-29 15:00:31 +0500

    card layout using react props function

--- src/components/props/CardLayoutStyle1.jsx
+++ src/components/props/CardLayoutStyle1.jsx
@@ -4,12 +4,13 @@ import { CardDetails } from './Card'
 function CardLayoutStyle1() {
 	return (
 		<section className='cardPreview'>
+			<h1>Cards Style</h1>
+			<p>This is card layout example where I use <strong>React Props</strong> for card details information</p>
 			<div className="mq-container">
-				<h2>Cards Style</h2>
 				<CardDetails imgPath="./assets/images/card-1.png" title="Texan Steak House Delivery" type="Web Design" />
-				<CardDetails imgPath="./assets/images/card-2.png" title="Make a Wish with Dream Whip" type="Facebook App Design" />
-				<CardDetails imgPath="./assets/images/card-1.png" title="Texan Steak House Delivery" type="Web Design" />
-				<CardDetails imgPath="./assets/images/card-2.png" title="Make a Wish with Dream Whip" type="Facebook App Design" />
+				<CardDetails imgPath="./assets/images/card-2.png" title="Swells" type="Mobile App Design" />
+				<CardDetails imgPath="./assets/images/card-3.png" title="Make a Wish with Dream Whip" type="Facebook App Design" />
+				<CardDetails imgPath="./assets/images/card-4.png" title="Lifetime Furniture" type="Web Design" />
 			</div>
 		</section>
 	)	
